0967183d9341bbdb1b01a60291ddfc675d66b244
Yousearches <Y.Hamdan1@protonmail.com>
2024-01-09 12:47:57 -0500

    some code

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,3 +1,12 @@
 import random 
 import string 
 import secrets 
+
+def passwordgenerator (minimum_length,numbers=True , special_caracter=True):
+    letters=string.ascii_letters
+    digits=string.digits
+    special_caracter=string.punctuation
+
+    print(letters,digits,special_caracter)
+
+passwordgenerator(10)
